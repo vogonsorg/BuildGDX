cd4bdbaf7a3ca7ab916fc70b184a7f0f977e5613
m210 <m210-2007@mail.ru>
2020-02-14 11:29:35 +0300

    TODO normalize

--- core/src/ru/m210projects/Build/Engine.java
+++ core/src/ru/m210projects/Build/Engine.java
@@ -63,38 +63,71 @@ import com.badlogic.gdx.graphics.PixmapIO;
 public abstract class Engine {
 
 	/*
-	 * TODO: Software renderer: and the draw distance for voxel detail is really low
-	 * Software renderer: You might want to look at wall sprites. I noticed a lot of
-	 * them clipping through geometry in classic render Software renderer: Voxel is
-	 * not clipped by ceiling geometry
+	 * TODO:
+	 * Software renderer: and the draw distance for voxel detail is really low
+	 * Software renderer: You might want to look at wall sprites. I noticed a lot of them clipping through geometry in classic render
+	 * Software renderer: Voxel is not clipped by ceiling geometry
 	 * 
-	 * osdrows в сохранения конфига Туман зависит от разрешения экрана (Polymost)
-	 * History list for last used IP's (client could be better for multiplayer) or
-	 * copy paste IP if possible. brz фильтр broadcast Some sort of anti-aliasing
-	 * option. The NVidia control panel's anti-aliasing works, but it introduces
-	 * artifacting on voxels. бинд в консоль плохой перенос строк в консоли если
-	 * строк больше 2х оптимизировать Bsprintf сохранения в папку (почему то не
-	 * находит файл) render: некоторые горизонтальные модели не там где надо под
-	 * определенным углом пропадает спрайт 2д карта подглюкивает вылазиют полигоны
-	 * за скайбокс потолок floor-alignment voxels for maphack
+	 * osdrows в сохранения конфига
+	 * Туман зависит от разрешения экрана (Polymost)
+	 * History list for last used IP's (client could be better for multiplayer) or copy paste IP if possible.
+	 * brz фильтр 
+	 * broadcast
+	 * Some sort of anti-aliasing option. The NVidia control panel's anti-aliasing works, but it introduces artifacting on voxels.
+	 * бинд в консоль 
+	 * плохой перенос строк в консоли если строк больше 2х
+	 * оптимизировать Bsprintf
+	 * сохранения в папку (почему то не находит файл)
+	 * render: некоторые горизонтальные модели не там где надо под определенным
+	 * углом пропадает спрайт 2д карта подглюкивает вылазиют полигоны за скайбокс
+	 * потолок
+	 * floor-alignment voxels for maphack 
 	 * 
-	 * для шейдеров: затенения уровня в далеке(туман) прекэш вокселей - палитра
-	 * FadeScreen Проверить HRP модели для шейдеров Отключить GL туман для шейдеров
-	 * Отключить фильтрацию текстур для шейдеров Не работают текстуры в userepisode
+	 * для шейдеров:
+	 * затенения уровня в далеке(туман)
+	 * прекэш вокселей - палитра
+	 * FadeScreen
+	 * Проверить HRP модели для шейдеров
+	 * Отключить GL туман для шейдеров
+	 * Отключить фильтрацию текстур для шейдеров
+	 * Не работают текстуры в userepisode
 	 * 
-	 * GameAdapter TODO: SaveManager findSaves() kOpen external file -> native
-	 * bytebuffer (read only) Launcher start parameters
+	 * GameAdapter TODO:
+	 * SaveManager findSaves()
+	 * kOpen external file -> native bytebuffer (read only)
+	 * Launcher start parameters
 	 * 
 	 * 
-	 * Architecture: Engine messages filecache filegroup -> filecache (net) mmulti
-	 * audiomanger (BAudio) midimodule openal renderer polymost -> texturecache
-	 * software input keyboard -> input() gamepad -> gpmanager
+	 * Architecture:
+	 * 	Engine
+	 * 		messages
+	 * 		filecache
+	 * 		filegroup		-> 		filecache
+	 * 		(net) mmulti
+	 * 		audiomanger (BAudio)
+	 * 			midimodule
+	 * 			openal
+	 * 		renderer				
+	 * 			polymost	->    texturecache
+	 * 			software
+	 * 		input
+	 * 			keyboard -> input()
+	 * 			gamepad	-> gpmanager
+	 * 		
+	 * 		OnScreenDisplay
+	 * 			Console
+	 * 		loader
+	 * 			md2
+	 * 			md3
+	 * 			vox
+	 * 			wav
+	 * 		
 	 * 
-	 * OnScreenDisplay Console loader md2 md3 vox wav
-	 * 
-	 * 
-	 * Utils string handler def loader + common + scriptfile -> texturecache /
-	 * mdloader pragmas bithandler
+	 *  Utils
+	 *  	string handler
+	 *  	def loader + common + scriptfile	-> texturecache / mdloader
+	 *  	pragmas
+	 *  	bithandler
 	 */
 
 	public static final String version = "20.021"; // XX. - year, XX - month, X - build
