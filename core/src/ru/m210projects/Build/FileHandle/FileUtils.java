3acd35fc56668f44a5ebb5b6420eac8a96852cf8
m210 <m210-2007@mail.ru>
2020-03-20 16:54:33 +0300

    Double slash fix

--- core/src/ru/m210projects/Build/FileHandle/FileUtils.java
+++ core/src/ru/m210projects/Build/FileHandle/FileUtils.java
@@ -20,40 +20,35 @@ import java.io.File;
 
 public class FileUtils {
 
-	public static boolean isExtension(String filename, String ext)
-	{
+	public static boolean isExtension(String filename, String ext) {
 		return filename.endsWith("." + ext);
 	}
-	
-	public static String getExtension(String filename)
-	{
-		if(filename == null || filename.isEmpty())
+
+	public static String getExtension(String filename) {
+		if (filename == null || filename.isEmpty())
 			return null;
 		return filename.substring(filename.lastIndexOf('.') + 1);
 	}
-	
-	public static String getFullName(String path)
-	{
-		if(path != null) {
+
+	public static String getFullName(String path) {
+		if (path != null) {
 			int index = -1;
-			if((index = path.lastIndexOf(File.separator)) != -1 
-					|| (index = path.lastIndexOf("\\")) != -1 
+			if ((index = path.lastIndexOf(File.separator)) != -1 || (index = path.lastIndexOf("\\")) != -1
 					|| (index = path.lastIndexOf("/")) != -1)
 				path = path.substring(index + 1);
 		}
-		
+
 		return path;
 	}
-	
-	public static String getCorrectPath(String path)
-	{
-		if(path != null) {
-			String[] separators = { "/"/* - Linux separator*/, "\\" };
-			for(String separator : separators)
-				if(!separator.equals(File.separator) && path.contains(separator))
+
+	public static String getCorrectPath(String path) {
+		if (path != null) {
+			String[] separators = { "/"/* - Linux separator */, "\\" };
+			for (String separator : separators)
+				if (!separator.equals(File.separator) && path.contains(separator))
 					path = path.replace(separator, File.separator);
+			path = path.replaceAll("(\\\\){2,}", "$1");
 		}
-		
 		return path;
 	}
 }
